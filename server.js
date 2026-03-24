e6e959218aa855766269d55710f8c415201859d6
SaulDM <tu_correo_de_github@gmail.com>
2026-03-24 00:41:41 -0600

    actualizacion

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const app = require('./app');
-const { iniciarScheduler } = require('./config/scheduler');
 
 // ================================
 // 🚀 START SERVER
@@ -10,7 +9,6 @@ const PORT = process.env.PORT || 1234;
 
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en puerto ${PORT}`);
-  iniciarScheduler();
 });
 
 module.exports = app;
